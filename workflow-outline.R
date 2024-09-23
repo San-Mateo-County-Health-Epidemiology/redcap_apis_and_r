7e9df414083fb663f536df4ed8f60a8122df6f70
elizabethjump <ejump@smcgov.org>
2024-09-23 09:10:18 -0700

    adding some sample cleaning code

--- workflow-outline.R
+++ workflow-outline.R
@@ -15,8 +15,39 @@ library(redcapAPI)
 library(sendmailR)
 
 # set-up -------------------------------------------
-# fetch and import new data into REDCap ----------------------
-source("scheduled_scripts\\sourced_scripts\\fetch_and_import_ed_census_data.R")
+## fetch and import new data into REDCap ----
+api_response <- GET(url = url,
+                    add_headers(Authorization = key),
+                    encode = "json")
+
+### convert to flat file + do basic cleaning ----
+edvol_today <- jsonlite::fromJSON(rawToChar(api_response$content)) %>%
+  jsonlite::flatten() %>%
+  filter(name %in% hosp_names) %>%
+  select(name, matches("edvol")) %>%
+  rename(edvol = edVolume.value,
+         edvol_update = edVolume.updateTime) %>%
+  mutate(edvol = case_when(as.Date(edvol_update) == Sys.Date() ~ edvol,
+                           TRUE ~ NA_integer_, 
+                           as.Date(edvol_update) < Sys.Date() ~ NA_integer_),
+         edvol_update = case_when(!is.na(edvol) ~ edvol_update, 
+                                  TRUE ~ NA_character_),
+         across(matches("edvol"), ~ as.character(.x))) %>%
+  pivot_longer(names_to = "var",
+               values_to = "val",
+               cols = matches("edvol")) %>%
+  mutate(new_var = paste0(var, "_", name_new)) %>%
+  select(-c(name_new, var)) %>%
+  pivot_wider(names_from = new_var, values_from = val) %>%
+  select(record_id, date, everything()) %>%
+  select_if(!is.na(.))
+
+### send into REDCap ----
+import_data <- importRecords(redcap_con,
+                             data = edvol_today,
+                             overwriteBehavior = "normal",
+                             returnContent = "count",
+                             returnData = F)
 
 # make ED Census PDF -------------------------------
 today <- str_replace_all(Sys.Date(), "-", "_")
@@ -27,9 +58,6 @@ rmarkdown::render(input = "A:/source-path/ed_census_markdown_pdf.Rmd",
 
 # send email --------------------------------------
 ## connect to REDCap ----
-redcap_con <- redcapConnection(url = '', 
-                               token = '')
-
 ed_census <- exportRecordsTyped(redcap_con)
 
 ## wrangle todays data into a string for the email ----
